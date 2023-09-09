4deb569093a8a117b4efea3a769f260d7ba60478
bobbyjsx <ezealagodswill@gmail.com>
2023-09-09 12:21:56 +0100

    py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import datetime
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/api")
+async def user(slack_name: str, track: str):
+    current_day = datetime.datetime.utcnow().strftime('%A')
+    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    return {
+            "slack_name": f"{slack_name}",
+            "current_day": current_day,
+            "utc_time": utc_time,
+            "track": f"{track}",
+            "github_file_url": "https://github.com/bobbyjsx/HNGX-Stage-1-BE/blob/main/main.py",
+            "github_repo_url": "https://github.com/bobbyjsx/HNGX-Stage-1-BE",
+            "status_code": 200,
+            }
