1607c62b8ecdf35423837fb8af746589c7bc3001
bobbyjsx <ezealagodswill@gmail.com>
2023-09-09 13:17:35 +0100

    env

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 from main import app
+from dotenv import load_dotenv
+load_dotenv()
+import os
+# import subprocess
+
+
+# print("Generating requirements.txt...")
+# subprocess.run("pip freeze > requirements.txt", shell=True)
+# print("Requirements file generated.")
+
+PORT = int(os.getenv("PORT", default=10000))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
 
 
 
